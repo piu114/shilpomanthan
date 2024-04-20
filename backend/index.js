77a63d94fd96924cf11f413115cdf9f39b1121c3
piu114 <piupatra114@gmail.com>
2024-04-20 10:39:28 +0530

    formatting code

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,18 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(uri, 
-    {
-      useNewUrlParser: true,
-    }
-  )
-  .then(() => {
-    console.log("Successfully connected to the database");
-  })
-  .catch((err) => {
-    console.log("Could not connect to the database. Error...", err);
-    process.exit();
-  });
+mongoose.connect(uri,
+	{
+		useNewUrlParser: true,
+	}
+)
+	.then(() => {
+		console.log("Successfully connected to the database");
+	})
+	.catch((err) => {
+		console.log("Could not connect to the database. Error...", err);
+		process.exit();
+	});
 
 const app = express();
 
@@ -26,11 +26,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Server is running :D" });
+	res.json({ message: "Server is running :D" });
 });
 
 let PORT = 8080;
 require('./app/routes/app.routes.js')(app);
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+	console.log(`Server is listening on port ${PORT}`);
 });
